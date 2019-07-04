b498fd91147d2d6c1d5e9c05975a961d28b43c2c
melisa gocay <mgocay@knights.ucf.edu>
2019-07-04 16:56:29 -0400

    Final Roll A Ball Commit

--- Roll-A-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -27,6 +27,10 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis ("Vertical");
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
         rb.AddForce (movement * speed); 
+        if (Input.GetKey("escape"))
+        Application.Quit ();
+     
+
     }
     void OnTriggerEnter (Collider other) 
     {
